75ac5d1bb28db82e5ea6b1f029b5fe8192efbea9
harshit0x0 <hpandey8888@gmail.com>
2024-06-06 21:27:08 +0530

    removed proxy, added base url in fetch

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://puta-election-app-backend.onrender.com',
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://puta-election-app-backend.onrender.com',
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 })
