9ad4e96b6cc2c2f76dd215ad5c1e6747015b5080
Siddharth <siddharth6527@gmail.com>
2024-06-09 02:38:19 +0530

    feat: Login Tab Implemented

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,55 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import ResultTable from '../components/ResultTable';
+import React, { useEffect, useState } from "react";
+import ResultTable from "../components/ResultTable";
 
 const changeFormat = (arr) => {
-    return arr.reduce((acc, current) => {
-        const formattedPosition = current.position.toLowerCase().replace(/ /g, "");
-        acc[formattedPosition] = current.candidates;
-        return acc;
-    }, {});
-}
+  return arr.reduce((acc, current) => {
+    const formattedPosition = current.position.toLowerCase().replace(/ /g, "");
+    acc[formattedPosition] = current.candidates;
+    return acc;
+  }, {});
+};
 
 const Results = () => {
-    const [results, setResults] = useState({
-        president: [],
-        vicepresident: [],
-        generalsecretary: [],
-        secretary: [],
-        treasurer: []
-    });
+  const [results, setResults] = useState({
+    president: [],
+    vicepresident: [],
+    generalsecretary: [],
+    secretary: [],
+    treasurer: [],
+  });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const responses = await fetch('https://puta-election-app-backend.onrender.com/api/v1/candidates');
-            // const response = await fetch('http://localhost:3000/api/v1/candidates');
-            const fetchedData = await responses.json();
-            const data = changeFormat(fetchedData.data);
-            setResults(data);
-        }
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const responses = await fetch(
+        "https://puta-election-app-backend.onrender.com/api/v1/candidates"
+      );
+      // const response = await fetch('http://localhost:3000/api/v1/candidates');
+      const fetchedData = await responses.json();
+      const data = changeFormat(fetchedData.data);
+      setResults(data);
+    };
+    fetchData();
+  }, []);
 
-    console.log(results);
-    const myStyle = {
-        marginBottom: 60
-    }
+  console.log(results);
+  const myStyle = {
+    marginBottom: 60,
+  };
 
-    return (
-        <div>
-            <h2 className='my-5'>Voting Results</h2>
-            <div style={myStyle}>
-                <ResultTable title="President" results={results.president} />
-            </div>
-            <div style={myStyle}>
-                <ResultTable title="Vice President" results={results.vicepresident} />
-            </div>
-            <div style={myStyle}>
-                <ResultTable title="General Secretary" results={results.generalsecretary} />
-            </div>
-            {/* <ResultTable title="Secretary" results={results.secretary} /> */}
-            {/* <ResultTable title="Treasurer" results={results.treasurer} /> */}
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="my-5">Voting Results</h2>
+      <div style={myStyle}>
+        <ResultTable title="President" results={results.president} />
+      </div>
+      <div style={myStyle}>
+        <ResultTable title="Vice President" results={results.vicepresident} />
+      </div>
+      <div style={myStyle}>
+        <ResultTable
+          title="General Secretary"
+          results={results.generalsecretary}
+        />
+      </div>
+      {/* <ResultTable title="Secretary" results={results.secretary} /> */}
+      {/* <ResultTable title="Treasurer" results={results.treasurer} /> */}
+    </div>
+  );
 };
 
 export default Results;
