d6b71209159d6ca6d4c24621c73fb83e160ee056
Siddharth <112471142+Siddharth6527@users.noreply.github.com>
2024-06-12 19:19:01 +0530

    Update serverFunctions.js_URL_fix

--- src/utils/serverFunctions.js
+++ src/utils/serverFunctions.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000/api/v1";
-// const BASE_URL = "https://puta-election-app-backend.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://puta-election-app-backend.onrender.com/api/v1";
 
 export const addDataToServer = async (form) => {
     try {
