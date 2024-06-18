833a8c26c9b5a0593b90d166e12b33bf2c23b1de
Siddharth <siddharth6527@gmail.com>
2024-06-18 21:13:30 +0530

    fix: Deployed URL fix_2

--- src/utils/serverFunctions.js
+++ src/utils/serverFunctions.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000/api/v1";
-// const BASE_URL = "https://puta-election-app-backend.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://puta-election-app-backend.onrender.com/api/v1";
 
 const getToken = () => {
   const token = localStorage.getItem("authToken");
